34fbcc2ce9c24048b47233662d45150461e4613f
GAURAV722-ve <gsinghgv722@gmail.com>
2026-07-24 09:35:22 +0530

    Basic

---
+++ SEM-5th/DAY_01/index.js
@@ -0,0 +1,35 @@
+// console.log(a)
+// var a = 2;
+// console.log(typeof(a))
+// var a = "abc";
+// console.log(typeof(a));
+// let b = 10;
+// console.log(typeof(b))
+// let b = "abc";
+// console.log(typeof(b))
+
+// let a = 2;
+// console.log(typeof(a));
+// {
+//     let a = "2";
+//     a = 2+"2";
+//     console.log(a);  //Let are block scope.
+// }
+// console.log(a);
+// console.log(typeof(a));
+
+// var a = 2;
+// console.log(typeof(a));
+// {
+//     var a = "2";
+//     a = 2+"2";
+//     console.log(a);  //var are global scope.
+// }
+// console.log(a);
+// console.log(typeof(a));
+
+const greet = () =>{
+    console.log("Hello Gaurav");
+}
+
+greet();
