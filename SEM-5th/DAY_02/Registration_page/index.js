bf6603a7c8a5858c8ebdffc44ffaf750a8485934
GAURAV722-ve <gsinghgv722@gmail.com>
2026-07-24 10:31:32 +0530

    Registation page

--- SEM-5th/DAY_02/Registration_page/index.js
+++ SEM-5th/DAY_02/Registration_page/index.js
@@ -4,26 +4,41 @@ const password = document.getElementById("password");
 const btn = document.getElementById("btn");
 const students = document.getElementById("students");
 
+
 btn.addEventListener("click", (e) => {
     e.preventDefault();
-
+    
     const emailVal = email.value;
     const nameVal = name1.value;
     const passwordVal = password.value;
 
-    // Print on the console
     console.log(nameVal);
     console.log(emailVal);
     console.log(passwordVal);
 
-    const innerdiv = document.createElement("div");
+    if(nameVal === "" || emailVal === "" || passwordVal === ""){
+        alert("Please fill all the details.");
+        return;
+    }
 
-    innerdiv.innerText =
-        `${nameVal} ${emailVal} ${passwordVal}`;
 
-    students.appendChild(innerdiv);
+    const innerDiv = document.createElement("div");
+
+    innerDiv.classList.add("student-card");
+
+
+    innerDiv.innerHTML = `
+        <h3>${nameVal}</h3>
+        <p><strong>Email :</strong> ${emailVal}</p>
+        <p><strong>Password :</strong> ********</p>
+    `;
+
+
+    students.appendChild(innerDiv);
+
 
     name1.value = "";
     email.value = "";
     password.value = "";
+
 });
