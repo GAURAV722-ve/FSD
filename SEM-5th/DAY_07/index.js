c4a18e467d703c34a2391efec7fa36fe3176a67a
GAURAV722-ve <gsinghgv722@gmail.com>
2026-08-20 14:34:41 +0530

    DAY_07

---
+++ SEM-5th/DAY_07/index.js
@@ -0,0 +1,23 @@
+// const div = document.getElementById("root");
+
+// const h1 = document.createElement("h1");
+
+// h1.innerText = "Welcome to the fsd class !!";
+
+// div.append(h1);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// const h1 = React.createElement("h1",{}, "Welcome to the FSD class.!!");
+// root.render(h1);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const div = [React.createElement("div",{},
+    React.createElement("div",{},
+        React.createElement("span",{},"Content-01")
+    )),
+    React.createElement("div",{},
+    React.createElement("div",{},
+        React.createElement("span",{},"Content-02")
+    ))];
+root.render(div);
